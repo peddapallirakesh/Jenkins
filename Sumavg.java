2b4ae34df8dbbd043a5036d5ab22de580f71cd97
peddapallirakesh <205278286+peddapallirakesh@users.noreply.github.com>
2025-07-11 12:28:24 +0530

    Add files via upload

---
+++ Sumavg.java
@@ -0,0 +1,14 @@
+ class Sumavg{
+ public static void main(String args[]){
+ 
+ int sum=0;
+ for(i=0;i<=10;i++)
+ {
+ sum+=1;
+ double avg=sum/10;
+ System.out.println("sum:"+sum);
+ System.out.println("avg:"+avg);
+ }
+ }
+ }
+ 
